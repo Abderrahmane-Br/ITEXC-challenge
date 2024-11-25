6ea56c4d2429946050bd012b57371951b30b215e
Abderrahmane Brahim <114669652+Abderrahmane-Br@users.noreply.github.com>
2024-11-25 08:02:52 +0000

    created view switch buttons

--- src/pages/appointments/components/Toolbar.jsx
+++ src/pages/appointments/components/Toolbar.jsx
@@ -1,14 +1,14 @@
 import ViewButton from "./ViewButton";
-import SwitchIcon from "../../assets/switch.svg";
-import CalendarIcon from "../../assets/calendar.svg";
+import SwitchIcon from "../../../assets/switch.svg";
+import CalendarIcon from "../../../assets/calendar.svg";
 
 function Toolbar() {
     return (
         <div className="toolbar">
             {/* <DateSelector /> */}
             <div>
-                <ViewButton icon={CalendarIcon} />
-                <ViewButton icon={SwitchIcon} />
+                <ViewButton  icon={CalendarIcon} />
+                <ViewButton  icon={SwitchIcon} />
             </div>
         </div>
     )
